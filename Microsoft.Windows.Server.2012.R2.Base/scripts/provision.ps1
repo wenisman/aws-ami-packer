b8ae602d5912e6840f5ee15c2f4eaaae471fa596
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2017-06-05 11:20:40 +1000

    update to remove anything specific

--- Microsoft.Windows.Server.2012.R2.Base/scripts/provision.ps1
+++ Microsoft.Windows.Server.2012.R2.Base/scripts/provision.ps1
@@ -1,6 +1,14 @@
 # Disable Windows Updates
 cmd /c reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
 
+# turn off windows updates
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name WindowsUpdate
+New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name AU
+New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 1
+
+# turn off the windows firewall
+netsh advfirewall set allprofiles state off
+
 tzutil /s "AUS Eastern Standard Time"
 
 Install-WindowsFeature Web-Server
@@ -9,8 +17,8 @@ Install-WindowsFeature Web-App-Dev -IncludeAllSubFeature
 
 #Write-output "Installing Dot Net Framework"
 New-Item c:\temp -type directory
-$source="https://nexus.prod.sbet.com.au/nexus/content/repositories/thirdparty/au/wh/NDP461-KB3102436-x86-x64-AllOS-ENU/4.6.1/NDP461-KB3102436-x86-x64-AllOS-ENU-4.6.1.exe"
-$destination="C:\temp\dotnet.exe"
+$source="https://go.microsoft.com/fwlink/?linkid=848827"
+$destination="C:\temp\dotnet-core.exe"
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($source,$destination)
 Start-Process -FilePath $destination -ArgumentList "/q /norestart" -Wait -Verb RunAs
